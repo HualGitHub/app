c507e294aece307ec1906d497243668898313c70
HualGitHub <820327685@qq.com>
2016-03-28 17:36:34 +0800

    android studio测试提交

---
+++ first/src/main/java/com/zxh/project/first/MainActivity.java
@@ -0,0 +1,14 @@
+package com.zxh.project.first;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
